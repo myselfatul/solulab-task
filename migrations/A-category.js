0a9bd7c21b87ead56259eed3e18d29f980ab9bcc
myselfatul <atulverma.verma4@gmail.com>
2021-09-29 23:14:17 +0530

    category migration file

---
+++ migrations/A-category.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('category', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      category_name: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    },{
+      underscored: true,
+      freezeTableName: true
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('category');
+  }
+};
